449bb6bee6bca3399796a82cf45c73b3e8c48cda
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-06-17 11:53:06 +0100

    added starter spring files for backend

---
+++ shopping-haven-backend/src/main/java/com/jiedwardsCodes/shoppinghaven/ShoppingHavenApplication.java
@@ -0,0 +1,13 @@
+package com.jiedwardsCodes.shoppinghaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingHavenApplication.class, args);
+	}
+
+}
---
+++ shopping-haven-backend/src/test/java/com/jiedwardsCodes/shoppinghaven/ShoppingHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiedwardsCodes.shoppinghaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
